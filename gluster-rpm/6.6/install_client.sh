f2e913dab2193e49022c23f0d9109a732d80e571
redynasc <cheng.sun@aispeech.com>
2020-10-21 14:43:53 +0800

    up so

--- gluster-rpm/6.6/install_client.sh
+++ gluster-rpm/6.6/install_client.sh
@@ -12,11 +12,16 @@ rpm -ivh glusterfs-api-6.6-1.el7.x86_64.rpm
 rpm -ivh glusterfs-fuse-6.6-1.el7.x86_64.rpm
 rpm -ivh glusterfs-cli-6.6-1.el7.x86_64.rpm
 
-fs_cache_so_dir=/usr/lib64/glusterfs/6.6/xlator/performance/
-if [[ -f ${fs_cache_so_dir}fs-cache.so ]]; then
-    /bin/cp ${fs_cache_so_dir}fs-cache.so  -f ${fs_cache_so_dir}fs-cache-old.so
-fi
-rm -f ${fs_cache_so_dir}fs-cache.so
-/bin/cp fs-cache.so -f ${fs_cache_so_dir}
-chmod 755  ${fs_cache_so_dir}fs-cache.so
+performance_so_dir=/usr/lib64/glusterfs/6.6/xlator/performance/
+/bin/mv ${performance_so_dir}fs-cache.so -f ${performance_so_dir}fs-cache-old.so
+/bin/cp fs-cache.so -f ${performance_so_dir}
+chmod 755  ${performance_so_dir}fs-cache.so
+
+/bin/mv ${performance_so_dir}md-cache.so -f ${performance_so_dir}md-cache-old.so
+/bin/cp md-cache.so -f ${performance_so_dir}
+chmod 755  ${performance_so_dir}md-cache.so
+
+/bin/mv ${performance_so_dir}io-threads.so -f ${performance_so_dir}io-threads-old.so
+/bin/cp io-threads.so -f ${performance_so_dir}
+chmod 755  ${performance_so_dir}io-threads.so
 
