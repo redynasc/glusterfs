3d72f71975389757ff813d94707e581fbbb63fbe
redynasc <cheng.sun@aispeech.com>
2021-06-15 11:05:16 +0800

    change install scripte

--- gluster-rpm/6.6/install_server.sh
+++ gluster-rpm/6.6/install_server.sh
@@ -4,6 +4,7 @@
 cd `dirname $0`
 pwd
 
+yum install -y python2-prettytable rpcbind lvm2 libaio
 
 rpm -ivh userspace-rcu-0.10.0-3.el7.x86_64.rpm
 rpm -ivh glusterfs-server-6.6-1.el7.x86_64.rpm
@@ -20,4 +21,5 @@ fi
 rm -f ${glusterd_so_dir}glusterd.so
 /bin/cp glusterd.so -f ${glusterd_so_dir}
 chmod 755  ${glusterd_so_dir}glusterd.so
+systemctl  enable glusterd
 systemctl  restart glusterd
